30716126bb108bbb243f2c459e9e93165f3b0da2
Yoshimi Takuto <yocchantaku0127@gmail.com>
2023-07-15 16:26:19 +0900

    add base.html

--- website/views.py
+++ website/views.py
@@ -1,7 +1,23 @@
+from typing import Any, Dict
 from django.views.generic import TemplateView  #djangoがもともと用意しているview(TemplateViewを使うことでユーザー画面に表示させることができる)
 
 class IndexView(TemplateView):
     template_name = 'index.html'
     
+    def get_context_data(self):
+        ctxt = super().get_context_data()
+        ctxt['username'] = 'takuto'
+        return ctxt
+        
 class AboutView(TemplateView):
-    template_name = 'about.html'    
+    template_name = 'about.html'
+    
+    def get_context_data(self):
+        ctxt = super().get_context_data()
+        ctxt['num_servies'] = 3344555
+        ctxt['skils'] = [
+            "Ruby(RubyOnRails)",
+            "Python(Django)",
+            "Javascript(Jquery)"
+        ]
+        return ctxt 
