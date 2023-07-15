cd717976eec9628f98294d4a56c002a970c6f833
Yoshimi Takuto <yocchantaku0127@gmail.com>
2023-07-15 22:40:50 +0900

    update settings.py

--- config/settings.py
+++ config/settings.py
@@ -79,11 +79,21 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+import dj_database_url
+from dotenv import(
+    find_dotenv,
+    load_dotenv,
+)
+load_dotenv(find_dotenv())
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'defalt': dj_database_url.config(conn_max_age=600),
 }
 
 
